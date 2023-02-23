1aa82c2ea5d91f1e2e33e923ffc1290b42d92ce9
rolandm99 <manfulmweze99@gmail.com>
2023-02-23 11:41:48 +0200

    Add the image to the NFT using jsonkeeper

--- scripts/run.ts
+++ scripts/run.ts
@@ -10,8 +10,10 @@ async function main() {
   const elon = await ElonNFT.deploy();
  
   await elon.deployed();
- 
   console.log("ElonNFT deployed to:", elon.address);
+
+  let txn = await elon.mintNFT();
+  await txn.wait();
 }
  
 // We recommend this pattern to be able to use async/await everywhere
